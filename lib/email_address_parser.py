a8deb7143dfc9aa3ea62c3ffc71783cbd54a0f76
Marysah <Marysah2@gmail.com>
2025-11-23 11:37:41 +0300

    initial commit

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,15 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, emails):
+        self.emails = emails
+
+    def parse(self):
+        # Regex that matches valid email addresses
+        pattern = r"[A-Za-z0-9\._%+-]+@[A-Za-z0-9\.-]+\.[A-Za-z]{2,}"
+
+        # Find all emails in the string
+        found = re.findall(pattern, self.emails)
+
+        # Sort alphabetically, as required by the test
+        return sorted(found)
